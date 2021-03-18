66dc53ba28e97c72b9da3695f8473485d9708653
Spenser Wellen <spenserwellen@gmail.com>
2021-03-18 12:29:04 -0500

    initial commit

---
+++ src/pages/Home.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Home() {
+  return <div>hello from home page</div>;
+}
---
+++ src/pages/Rooms.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Rooms = () => {
+  return <div>hello from rooms page</div>;
+};
+
+export default Rooms;
