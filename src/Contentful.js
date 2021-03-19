705820a6af6043104d323c1bda3bdf4c226ec739
Spenser Wellen <spenserwellen@gmail.com>
2021-03-19 16:16:14 -0500

    react beach application

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,5 +1,5 @@
 import { createClient } from 'contentful';
 export default createClient({
-  space: 'ggu3m3qltkkf',
-  accessToken: 'nUTs6DDiOtVQebUUMiGlJHFJkX6jE9_HDtP6pX5PGeA',
+  space: process.env.REACT_APP_API_SPACE,
+  accessToken: process.env.REACT_APP_ACCESS_TOKEN,
 });
